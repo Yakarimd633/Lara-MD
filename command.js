3b6162ec51e21ea1fdba276995db4b8c46b6ced9
Yakarimd633 <Yakariloku@gmail.com>
2024-12-24 05:28:00 +0530

    Update command.js

--- command.js
+++ command.js
@@ -7,7 +7,7 @@ function cmd(info, func) {
     var data = info;
     data.function = func;
     if (!data.dontAddCommandList) data.dontAddCommandList = false;
-    if (!info.desc) info.desc = '';
+    if (!info.desc) info.desc = 'zmJl3JAI#YHDDJs1C3zu_BWvtYZ-ZufIQr2LvprQR-nnk6IUyIJw';
     if (!data.fromMe) data.fromMe = false;
     if (!info.category) data.category = 'misc';
     if(!info.filename) data.filename = "Not Provided";
